48c3bda20b892baeb54e85d05cae7da313541b7d
Mons Anderson <mons@cpan.org>
2017-12-12 02:06:52 +0300

    Spec and TODO

---
+++ string-h-scm-1.rockspec
@@ -0,0 +1,26 @@
+package = 'string-h'
+version = 'scm-1'
+
+source  = {
+    url    = 'git://github.com/moonlibs/string-h.git';
+    branch = 'master';
+}
+
+description = {
+    summary  = "LuaJIT ffi interface to string.h functions";
+    detailed = "LuaJIT ffi interface to string.h functions";
+    homepage = 'https://github.com/moonlibs/string-h';
+    license  = 'Artistic';
+    maintainer = "Mons Anderson <mons@cpan.org>";
+}
+
+dependencies = {
+    'lua >= 5.1';
+}
+
+build = {
+    type = 'builtin',
+    modules = {
+        ['string.h'] = 'string/h.lua';
+    }
+}
--- string/h.lua
+++ string/h.lua
@@ -1,5 +1,44 @@
 local ffi = require 'ffi'
 
+--[[
+
+1. test wether we could modify lua strings with mem*
+void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+void *memset(char *, int z, size_t);
+char *strcpy(char *toHere, const char *fromHere);
+char *strncpy(char *toHere, const char *fromHere, size_t n);
+
+2. TODO: define signature and implement
+
+void *memchr(const char *s, char c, size_t n);
+void *strchr(const char *s, char c, size_t n);
+char *strrchr(const char *s, int c);
+int strcoll(const char *, const char *);
+size_t strspn(const char *s, const char *accept);
+size_t strcspn(const char *s, const char *reject);
+char *strpbrk(const char *s, const char *accept);
+
+3. Requres alloc:
+size_t strxfrm(char *dest, const char *src, size_t n);
+char *strtok(char *, const char *);
+char *strtok_r(char *, const char *, char **); -- POSIX
+
+4. Do we need this? Think no
+char *strcat(char *dest, const char *src);
+char *strncat(char *dest, const char *src, size_t n);
+
+5. Inherit from ffi or bare lua
+char *strerror(int);
+size_t strlen(const char *);
+
+6. Can't be used in lua
+char *strdup(const char *);
+
+
+]]
+
+
 -- reload protect
 local M = rawget(_G,'\0string.h')
 if M then return M end
@@ -41,6 +80,10 @@ function M.strcasestr(haystack, needle)
 	else
 		return
 	end
+end
+
+function M.memcmp(s1,s2)
+	return tonumber(C.memcmp(ffi.cast(char_p,s1),ffi.cast(char_p,s2)))
 end
 
 function M.strcmp(s1,s2)
